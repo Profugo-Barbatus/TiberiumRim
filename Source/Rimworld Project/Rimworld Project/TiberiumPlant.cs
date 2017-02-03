a008d73d21fd7b94ad03747542900e65fbc7d955
Profugo-Barbatus <chefsbrian@gmail.com>
2017-02-03 08:58:21 -0700

    Tiberium Inhibitor
    Addition of Tiberium Inhibitor code.
    
    New XML:
    New CompProperties_SonicInhibitor. This can be attached to *any*
    building to give it a tiberium destructive effect. This will cause it to
    both destroy tiberium crystals, and tiberium plants in the given radius.
    Specifically, any plant containing "Tiberium" in the name will be
    destroyed. Does not detonate blue tiberium.

--- Source/Rimworld Project/Rimworld Project/TiberiumPlant.cs
+++ Source/Rimworld Project/Rimworld Project/TiberiumPlant.cs
@@ -458,14 +458,14 @@ namespace TiberiumRim
                         {
                             if(!friendlyTo.Contains(p.def))
                             {
-                                p.Destroy(DestroyMode.Kill);
+                                p.Destroy(DestroyMode.Vanish);
                             }
                         }
                         //Otherwise, regular behavior to avoid self-killing
                         else if (p.def != plantDef)
                         {
                             //Kill the plant. Later, we'll consider a piece of tiberium infected plantlife in its place.
-                            p.Destroy(DestroyMode.Kill);
+                            p.Destroy(DestroyMode.Vanish);
                         }
                     }
                 }
