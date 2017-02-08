b4d61c478e11c68a39c598b13d670be300aa3f47
Profugo-Barbatus <chefsbrian@gmail.com>
2017-02-08 13:23:30 -0700

    Change Request Revisions
    Requested changes for merge commit.
    
    Alterations:
    Tiberium fauna changed from 30% spawn chance to 5% spawn chance.
    Missing crawler case added.
    Commented out debug lines. Need to build a proper debug Manager class
    one of these days.

--- Source/Rimworld Project/Rimworld Project/TiberiumPlant.cs
+++ Source/Rimworld Project/Rimworld Project/TiberiumPlant.cs
@@ -362,6 +362,10 @@ namespace TiberiumRim
                         creature = DefDatabase<PawnKindDef>.GetNamed("TiberiumFiend_TBI", true);
                         break;
 
+                    case "Snake":
+                        creature = DefDatabase<PawnKindDef>.GetNamed("Crawler_TBI", true);
+                        break;
+
                     default:
                         creature = DefDatabase<PawnKindDef>.GetNamed("Visceroid_TBI", true);
                         break;
@@ -462,7 +466,7 @@ namespace TiberiumRim
                         {
                             if(!friendlyTo.Contains(p.def))
                             {
-                                if (Rand.Chance(0.3f))
+                                if (Rand.Chance(0.05f))
                                 {
                                     ThingDef flora = DefDatabase<ThingDef>.GetNamed("TiberiumPlant", true);
                                     IntVec3 loc = p.Position;
@@ -483,7 +487,7 @@ namespace TiberiumRim
                         {
 
                             //Kill the plant. Later, we'll consider a piece of tiberium infected plantlife in its place.
-                            if (Rand.Chance(0.3f))
+                            if (Rand.Chance(0.05f))
                             {
                                 ThingDef flora = DefDatabase<ThingDef>.GetNamed("TiberiumPlant", true);
                                 IntVec3 loc = p.Position;
